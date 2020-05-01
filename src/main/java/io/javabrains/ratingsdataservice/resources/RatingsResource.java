65e3f6664dcb457b675754d40f45d7caf84689d1
School <puck.school@atos.net>
2020-05-01 15:41:28 +0200

    add logger

--- src/main/java/io/javabrains/ratingsdataservice/resources/RatingsResource.java
+++ src/main/java/io/javabrains/ratingsdataservice/resources/RatingsResource.java
@@ -7,23 +7,27 @@ import io.javabrains.ratingsdataservice.model.Rating;
 import io.javabrains.ratingsdataservice.model.RatingList;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.logging.Logger;
 
 @RestController
 @RequestMapping("/ratingsdata")
 public class RatingsResource {
 
+    private static final Logger LOGGER = Logger.getLogger( RatingsResource.class.getName() );
+
     final String uri = "http://www.omdbapi.com/?t=%s&apikey=cc323c12";
 
-    @RequestMapping("/movies/{movieTitle}")
+    @RequestMapping(path = "/movies/{movieTitle}", method = {RequestMethod.GET})
     public Rating getMovieRating(@PathVariable("movieTitle") String movieTitle) {
         return new Rating(movieTitle, "5");
     }
 
-    @RequestMapping("/user/{movieTitle}")
+    @RequestMapping(path = "/user/{movieTitle}", method = {RequestMethod.GET})
     public RatingList getUserRatings(@PathVariable("movieTitle") String movieTitle) {
         RatingList ratingList = new RatingList();
         RestTemplate restTemplate = new RestTemplate();
@@ -37,7 +41,7 @@ public class RatingsResource {
             ratingList.setRatings(Arrays.asList(list));
             ratingList.setMovieTitle(movieTitle);
         } catch (IOException e) {
-            e.printStackTrace();
+            LOGGER.log(e);
         }
         return ratingList;
     }
