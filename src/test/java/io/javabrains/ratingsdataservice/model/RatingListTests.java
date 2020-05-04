c337672917d147f63ecb7946fdfde071da39e2d2
School <puck.school@atos.net>
2020-05-04 15:17:02 +0200

    adjust unit-test for RatingList

--- src/test/java/io/javabrains/ratingsdataservice/model/RatingListTests.java
+++ src/test/java/io/javabrains/ratingsdataservice/model/RatingListTests.java
@@ -1,6 +1,7 @@
 package io.javabrains.ratingsdataservice.model;
 
 import io.javabrains.ratingsdataservice.PojoTestUtils;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class RatingListTests {
@@ -8,5 +9,15 @@ public class RatingListTests {
     @Test
     public void validateUserRatingGettersSettersTest() {
         PojoTestUtils.ValidateAccessors(RatingList.class);
+    }
+
+    @Test
+    public void validateGetMovieTitle() {
+        String movieTitle = "Scary movie";
+
+        RatingList ratingList = new RatingList();
+        ratingList.setMovieTitle(movieTitle);
+
+        Assert.assertEquals(movieTitle, ratingList.getMovieTitle());
     }
 }
