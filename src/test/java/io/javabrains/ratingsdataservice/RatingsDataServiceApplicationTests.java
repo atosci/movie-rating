2b46ad2e00f1ceef9e9e6d4ed71b44f81b2c30a4
School <puck.school@atos.net>
2020-05-04 16:23:24 +0200

    fix for code coverage

--- src/test/java/io/javabrains/ratingsdataservice/RatingsDataServiceApplicationTests.java
+++ src/test/java/io/javabrains/ratingsdataservice/RatingsDataServiceApplicationTests.java
@@ -2,6 +2,7 @@ package io.javabrains.ratingsdataservice;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -11,6 +12,7 @@ public class RatingsDataServiceApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		RatingsDataServiceApplication.main(new String[] {});
 	}
 
 }
