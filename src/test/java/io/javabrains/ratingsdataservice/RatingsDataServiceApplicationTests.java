41550cc3a46eecfd1b3a4705f689b2fc18126430
School <puck.school@atos.net>
2020-05-05 13:46:51 +0200

    update unit-test

--- src/test/java/io/javabrains/ratingsdataservice/RatingsDataServiceApplicationTests.java
+++ src/test/java/io/javabrains/ratingsdataservice/RatingsDataServiceApplicationTests.java
@@ -1,7 +1,10 @@
 package io.javabrains.ratingsdataservice;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
