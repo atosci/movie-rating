2254cb67c7c62114e62ae4bc46d9d748d0cb2295
PCSchool <puckkschool@gmail.com>
2020-05-13 09:56:30 +0200

    add JavaDoc comments

--- src/main/java/io/atos/ratingsdataservice/model/Rating.java
+++ src/main/java/io/atos/ratingsdataservice/model/Rating.java
@@ -1,7 +1,10 @@
 package io.atos.ratingsdataservice.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-
+/**
+ * @author Mohamed El Kawakibi
+ * @author Puck School
+ */
 public class Rating {
 
     @JsonProperty("Source")
@@ -14,23 +17,40 @@ public class Rating {
     public Rating() {
     }
 
+
+    /**
+     * @param source A String representing the source of the rating
+     * @param value A String representing the value of the rating
+     */
     public Rating(String source, String value) {
         this.source = source;
         this.value = value;
     }
 
+    /** Gets the source of the rating
+     * @return A String representing the rating source
+     */
     public String getSource() {
         return source;
     }
 
+    /** Sets the source of the rating
+     * @param source String representing the rating source
+     */
     public void setSource(String source) {
         this.source = source;
     }
 
+    /** Gets the value of the rating
+     * @return A String representing the rating value
+     */
     public String getValue() {
         return value;
     }
 
+    /** Sets the value of the rating
+     * @param value String representing the rating value
+     */
     public void setValue(String value) {
         this.value = value;
     }
--- src/main/java/io/atos/ratingsdataservice/resources/RatingsResource.java
+++ src/main/java/io/atos/ratingsdataservice/resources/RatingsResource.java
@@ -10,7 +10,10 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
+/**
+ * @author Mohamed El Kawakibi
+ * @author Puck School
+ */
 @RestController
 @RequestMapping("/ratingsdata")
 public class RatingsResource {
